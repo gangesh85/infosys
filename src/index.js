9bc148a343ccb6d9ec8081aeeae087bf86a81ea5
gangesh85 <gangesh30624@gmail.com>
2024-10-23 08:00:02 +0530

    add logger

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+LogRocket.init('9cht1a/rewards');
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
